c31e242ac4dbff07c99526834d2ca4914f6b00d9
Vitaly Bogachev <vit.bogachev@gmail.com>
2019-11-05 23:55:10 +0300

    Add validates :text length

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
 
-  validates :text, :user, presence: true, :text, length: { maximum: 255 }
+  validates :text, :user, presence: true
+  validates :text, length: { maximum: 255 }
 end
