56842b134f1e4eed04da4ed5e59b39c8ab141f01
Vitaly Bogachev <vit.bogachev@gmail.com>
2019-11-06 22:12:52 +0300

    Add migrate AddIndexToUser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_220118) do
+ActiveRecord::Schema.define(version: 2019_11_06_145219) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_11_03_220118) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
