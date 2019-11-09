cadb8a1537de79c54d6362ff5a0e3199ad033a62
Vitaly Bogachev <vit.bogachev@gmail.com>
2019-11-09 22:26:46 +0300

    Add views and styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
+  # Это действие отзывается, когда пользователь заходит по адресу /users
+  def index
+    # Мы создаем массив из двух болванок пользователей. Для создания фейковой
+    # модели мы просто вызываем метод User.new, который создает модель, не
+    # записывая её в базу.
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Vasiliy',
+            username: '@vasko',
+            avatar_url: 'http://avatarmaker.ru/img/11/1020/101960.jpg'
+        ),
+        User.new(id: 2, name: 'Ledger', username: 'joker')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = 'Привет'
+    @user = User.new(
+      name: 'Vasiliy',
+      username: 'vasko',
+      avatar_url: 'http://avatarmaker.ru/img/11/1020/101960.jpg'
+    )
+
+    @questions = [
+      Question.new(text: 'Как вас зовут?', created_at: Date.parse('01.11.2019')),
+      Question.new(text: 'Из какого вы города?', created_at: Date.parse('02.11.2019'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # Это правило говорит: если пользователь заходит по адресу /, направь его в
+  # контроллер users, действие index. Грубо говоря, на главной странице у нас
+  # список юзеров.
+  root 'users#index'
+
+  resources :users
+  resource :questions
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'show' => 'users#show'
