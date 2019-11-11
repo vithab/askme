7403ae3fe8b0f6fd4181c0a1a0d205f73a128087
Vitaly Bogachev <vit.bogachev@gmail.com>
2019-11-11 23:50:05 +0300

    Add UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,20 @@ Rails.application.routes.draw do
   # список юзеров.
   root 'users#index'
 
-  resources :users
-  resource :questions
+  # Ресурс пользователей (экшен destroy не поддерживается)
+  resources :users, except: [:destroy]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Ресурс сессий (только три экшена :new, :create, :destroy)
+  resources :sessions, only: [:new, :create, :destroy]
 
-  get 'show' => 'users#show'
+  # Ресурс вопросов (кроме экшенов :show, :new, :index)
+  resources :questions, except: [:show, :new, :index]
+
+  # Синонимы путей — в дополнение к созданным в ресурсах выше.
+  #
+  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
+  # их удалим.
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
