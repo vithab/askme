1d35b56fccb2baaa2492566d6839100b3d795c89
Vitaly Bogachev <vit.bogachev@gmail.com>
2019-11-14 11:55:38 +0300

    Authors of questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   validates :text, :user, presence: true
   validates :text, length: { maximum: 255 }
